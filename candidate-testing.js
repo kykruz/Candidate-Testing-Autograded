6cce2b581b7dbb124746d910af5728c073269687
kykruz <k.kruzan@yahoo.com>
2024-03-29 19:05:49 -0500

    GA part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -26,7 +26,7 @@ function askForName() {
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 for (let i=0; i < questions.length; i++) {
-  candidateAnswers[i] = console.log('');input.question(questions[i]);
+  candidateAnswers[i] = input.question(questions[i]); console.log("")
 }
 
 }
@@ -53,7 +53,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Hello, " + candidateName + "!");
+   console.log("Hello, " + candidateName + "!\n");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
